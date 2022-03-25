36ddd08b923470123d0b64ca86734ffe0cbd772c
unknown <lawrenceleemy@gmail.com>
2022-03-26 00:30:48 +0800

    WordsToken & StopWord process

---
+++ WordToken_StopWord.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import nltk
+
+all_stopwords = [',','!',':','?','.','%','-','i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
+df = pd.read_csv('trec07p(v1).csv')
+body1 = df['body']
+y1 = df['y']
+body66=[]     
+for i in range(len(body1)):
+    word_data=""
+    word_data=body1[i]
+    nltk_tokens = nltk.word_tokenize(str(word_data))
+    body2 = [word for word in nltk_tokens if not word in all_stopwords]
+    y2 = y1[i]
+    if len(body2) > 0:
+        body66.append([body2,y2])
+    
+columns = ['body','y']
+df1 = pd.DataFrame(body66, columns=columns)
+
+df1.to_csv('Word Tokenization1(v2).csv', index=False)
